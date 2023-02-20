ed8b96ea9ea33f8e4db94f4652a009add0aa5550
Håvard <h.lundheim@gmail.com>
2023-02-20 15:42:29 +0100

    første commit

---
+++ MPU_hypemeter_bra/MPU_hypemeter_bra.ino
@@ -0,0 +1,76 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <FastLED.h>
+#define LED_PIN 15
+#define NUM_LEDS 10
+#define led 2 //Gir pin 2 navnet led.
+
+CRGB leds[NUM_LEDS];
+
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+  pinMode(led,OUTPUT);
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+    
+  Serial.println("Hypemeter test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("aaah lets go da");
+
+  Serial.println("");
+  delay(100);
+
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(3, 300);
+  FastLED.clear();
+  FastLED.show();
+  for (int i = 0; i<NUM_LEDS;i++) {
+ 
+  leds[i] = CRGB(0, 0, 255 );
+  }
+}
+
+float getHype(sensors_event_t a){
+  return abs(sqrt(pow(a.acceleration.x,2) + pow(a.acceleration.y,2)+ pow(a.acceleration.z,2))-9);
+}
+
+void loop() {
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  float hype = getHype(a);
+  
+  /*if (hype > 18){
+    digitalWrite(led,HIGH); //sender ut signalet "høy" til pin 2(led), LED-lyset vil da lyse.
+  } else {
+     digitalWrite(led,LOW); //sender ut signalet "lav" til pin 2 (led), LED-lyset blir skrudd av.
+  }*/
+
+
+  FastLED.setBrightness((hype)*3);
+  FastLED.show();
+  
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("HYPE:");
+  Serial.println(hype);
+  delay(30);
+}
