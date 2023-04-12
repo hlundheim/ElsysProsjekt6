1dfff52f369a694b114fa8da992b557c6f8f0a01
Håvard <h.lundheim@gmail.com>
2023-04-12 10:43:56 +0200

    oppdatert esp kode

--- MPU_hypemeter_bra/MPU_hypemeter_bra.ino
+++ MPU_hypemeter_bra/MPU_hypemeter_bra.ino
@@ -4,9 +4,8 @@
 #include <FastLED.h>
 #include <HTTPClient.h>        
 #include <WiFi.h>  
-#define ledPin 15
+#define ledPin 13
 #define numLeds 10
-#define EN 33
 #define VIB 32
 #define ENV 34
 int noiseLevel = 0;
@@ -15,7 +14,8 @@ Adafruit_MPU6050 mpu;
 const char* ssid = "OnePlus 5";       
 const char* password = "ohanaelsys6"; 
 float hype;
-float biggestHype = 5;
+const float baseHype = 5;
+float biggestHype = baseHype;
 String dataToSend = "";
 String effectData = "255,000,255,1,150,110,050,0,0,0";
 TaskHandle_t wifi;
@@ -35,15 +35,15 @@ float getHype(sensors_event_t a){
 }
 
 void getColor(){
-  String red1 = effectData.substring(0,2);
-  String green1 = effectData.substring(4,6);
-  String blue1 = effectData.substring(8,10);
-  multiColor = effectData.substring(12,12).toInt();
-  String red2 = effectData.substring(14,16);
-  String green2 = effectData.substring(18,20);
-  String blue2 = effectData.substring(22,24);
-  alternating = effectData.substring(26,26).toInt();
-  rainbow = effectData.substring(28,28).toInt();
+  String red1 = effectData.substring(0,3);
+  String green1 = effectData.substring(4,7);
+  String blue1 = effectData.substring(8,11);
+  multiColor = effectData.substring(12,13).toInt();
+  String red2 = effectData.substring(14,17);
+  String green2 = effectData.substring(18,21);
+  String blue2 = effectData.substring(22,25);
+  alternating = effectData.substring(26,27).toInt();
+  rainbow = effectData.substring(28,29).toInt();
   if(altFlag == 0) {
     color[0] = red1.toInt(); 
     color[1] = green1.toInt(); 
@@ -67,19 +67,22 @@ void getColor(){
 }
 
 void setup(void) {
-  pinMode(EN, OUTPUT);
   pinMode(VIB, OUTPUT);
   pinMode(ENV, INPUT);
-  digitalWrite(EN, HIGH);
 
   Serial.begin(115200);
   Serial.println("ESP test!");
-  
-  WiFi.begin(ssid, password);             //Start wifi connection
+
+  int connectCounter = 0;
+  WiFi.begin(ssid, password);             
   Serial.print("Connecting...");
-  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+  while (WiFi.status() != WL_CONNECTED) { 
     delay(500);
     Serial.print(".");
+    connectCounter++;
+    if (connectCounter > 10) {
+      ESP.restart();
+    }
   }
   Serial.print("Connected, my IP: ");
   Serial.println(WiFi.localIP());
@@ -96,12 +99,10 @@ void setup(void) {
   Serial.println("");
   
   FastLED.addLeds<WS2812, ledPin, GRB>(leds, numLeds);
-  FastLED.setMaxPowerInVoltsAndMilliamps(3, 300);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 300);
   FastLED.clear();
   FastLED.show();
-  // for (int i = 0; i<numLeds;i++) {
-  // leds[i] = CRGB(0, 0, 255 );
-  // }
+
     xTaskCreatePinnedToCore(
       wifiHandler,   // Task function. 
       "wifi",     // name of task. 
@@ -121,34 +122,27 @@ void wifiHandler( void * pvParameters ){
   Serial.println(xPortGetCoreID());
 
   for(;;){
-      if(WiFi.status()== WL_CONNECTED){                   //Check WiFi connection status  
+      if(WiFi.status()== WL_CONNECTED){                
       HTTPClient http;  
-      //Create new client
-      dataToSend = "check_LED_status=1";    //If button wasn't pressed we send text: "check_LED_status"
-      
-      //Begin new connection to website     
-      http.begin("https://ohana6.000webhostapp.com/esp32_update.php");   //Indicate the destination webpage 
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");         //Prepare the header
-      int responseCode = http.POST(dataToSend);                                //Send the POST. This will giveg us a response code Denne tar fakka lang tid
-      //If the code is higher than 0, it means we received a response
+      dataToSend = "check_LED_status=1";   
+          
+      http.begin("https://ohana6.000webhostapp.com/esp32_update.php");
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");  
+      int responseCode = http.POST(dataToSend);                
       if(responseCode > 0){
-        Serial.println("HTTP code " + String(responseCode));                     //Print return code
-        if(responseCode == 200){                                                 //If code is 200, we received a good response and we can read the echo data
-          String responseBody = http.getString();                                //Save the data comming from the website
+        Serial.println("HTTP code " + String(responseCode));                    
+        if(responseCode == 200){                                                 
+          String responseBody = http.getString();                               
           effectData = responseBody;
-          Serial.print("Server reply: ");                                         //Print data to the monitor for debug
-          Serial.println(responseBody);
-                  
-        }//End of responseCode = 200
-      }//END of responseCode > 0
-      
-      else{
+          Serial.print("Server reply: ");                           
+          Serial.println(responseBody);    
+        }
+      } else{
        Serial.print("Error sending POST, code: ");
        Serial.println(responseCode);
       }
       http.end();                                                                 //End the connection
-    }//END of WIFI connected
-    else{
+    } else{
       Serial.println("WIFI connection error");
       delay(500);
     }
@@ -156,39 +150,53 @@ void wifiHandler( void * pvParameters ){
   } 
 }
 
-
 void loop() {
   noiseLevel = analogRead(ENV);
-  analogWrite(VIB, noiseLevel/5);
+  analogWrite(VIB, noiseLevel/3);
   
   sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp); //Prøv ut å fikse litt greier her kanskje add større delay eller noe
+  mpu.getEvent(&a, &g, &temp); 
   delay(30);
   hype = getHype(a);
 
   if (hype > biggestHype) {
     biggestHype = hype;
+  } else if (hype > baseHype) {
+    biggestHype -= 0.01;
   }
+  
   if (millis()-previousMillis > 500) {
       getColor();
       previousMillis = millis();  
   }
-  if(rainbow) {
-    
-  }
-  for (int i = 0; i<numLeds;i=i+2) {
+ 
+  for (int i = 0; i<numLeds;i++) {
     leds[i] = CRGB(color[0], color[1], color[2]);
   }
   if(multiColor) {
-    for (int i = 1; i<numLeds-1;i=i+2) {
+    for (int i = 0; i<numLeds;i=i+2) {
     leds[i] = CRGB(color2[0], color2[1], color2[2]);
     }
   }
 
-  FastLED.setBrightness(10 + hype*245/biggestHype);
+  FastLED.setBrightness(30 + hype*225/biggestHype);
   FastLED.show();
 
-  Serial.print(effectData);
+  /*Serial.println(effectData);
+  Serial.print("color1 ");
+  Serial.print("\t");
+  Serial.print(color[0]);
+  Serial.print("\t");
+  Serial.print(color[1]);
+  Serial.print("\t");
+  Serial.println(color[2]);
+  Serial.print("color2");
+  Serial.print("\t");
+  Serial.print(color2[0]);
+  Serial.print("\t");
+  Serial.print(color2[1]);
+  Serial.print("\t");
+  Serial.println(color2[2]); */
   Serial.print(" HYPE:");
   Serial.println(hype);
   // Serial.print("Acceleration X: ");
@@ -198,6 +206,6 @@ void loop() {
   // Serial.print(", Z: ");
   // Serial.print(a.acceleration.z);
   // Serial.println(" m/s^2");
-  Serial.print("noiseLevel");
+  Serial.print("noiseLevel ");
   Serial.println(noiseLevel);
 }
